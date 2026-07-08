cb01c33a2797a73e4676a9d907cab3d505e8f2c0
varnit7002-tech <varnit7002@gmail.com>
2026-07-08 13:06:22 +0530

    js trying logic

--- first.js
+++ first.js
@@ -1,6 +1,57 @@
 function startQuiz() {
     document.getElementById("start-screen").style.display = "none";
-
     document.getElementById("game-screen").style.display = "flex";
+   loadQuestion();
 }
+ const quizData = [   
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: 2
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: 1
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        answer: 3
+    }   
+]
+let currentQuestion = 0;
+
+
+
+
+function loadQuestion() {
+    const currentQuiz = quizData[currentQuestion];
+
+    document.getElementById("question").textContent =
+        currentQuiz.question;
+
+    const buttons = document.querySelectorAll(".btn");
+
+    buttons[0].textContent = currentQuiz.options[0];
+    buttons[1].textContent = currentQuiz.options[1];
+    buttons[2].textContent = currentQuiz.options[2];
+    buttons[3].textContent = currentQuiz.options[3];
+
+  
+}
+
+function checkAnswer(checkinganswer) {
+    const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function(button, index) {
+    button.addEventListener("click", function() {
+        checkAnswer(index);
+    });
+});
+
+
+}
+
+
 
